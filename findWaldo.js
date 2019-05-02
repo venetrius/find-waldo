a9748e4d35cc2befa58a595997c887b8009807f5
venetrius <gergelygjuhasz@gmail.com>
2019-05-02 18:51:33 +0000

    using Array.prototype.forEach()

--- findWaldo.js
+++ findWaldo.js
@@ -1,10 +1,12 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(
+    function(element,index){
+      if(element == "Waldo"){
+        found(index);
+      }
     }
-  }
+  );
 }
 
 function actionWhenFound(index) {
